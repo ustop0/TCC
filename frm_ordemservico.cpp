a08fa2ed42e646ef9cdfd448d81736fc36abb5c8
Thiago_Ianzer <thiagoianzer@gmail.com>
2022-06-01 15:05:53 -0300

    desenvolvendo tela de OS

--- frm_ordemservico.cpp
+++ frm_ordemservico.cpp
@@ -1,14 +1,28 @@
 #include "frm_ordemservico.h"
 #include "ui_frm_ordemservico.h"
+#include "frms_selecionacliente.h" //formulario de seleção de clientes
 
-frm_ordemservico::frm_ordemservico(QWidget *parent) :
+frm_ordemservico::frm_ordemservico(QWidget *parent, QString c_codigo_cliente
+                                                  , QString c_nome_cliente
+                                                  , QString c_codigo_veiculo
+                                                  , QString c_nome_veiculo) :
     QDialog(parent),
     ui(new Ui::frm_ordemservico)
 {
     ui->setupUi(this);
+
+    //abrindo a conexao com o banco
+    if( !con.abrir() ) //verificando se a conexao foi aberta
+    {
+        if( !con.abrir() )
+        {
+            QMessageBox::warning(this, "ERRO", "Erro ao abrir banco de dados");
+        }
+    }
 }
 
-frm_ordemservico::~frm_ordemservico()
+frm_ordemservico::~frm_ordemservico() //**INICIO** destrutor
 {
+    con.fechar();
     delete ui;
 }
--- frm_ordemservico.h
+++ frm_ordemservico.h
@@ -2,6 +2,20 @@
 #define FRM_ORDEMSERVICO_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QDebug>
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QMainWindow>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QObject> //sinais entre forms
+#include "ConexaoBanco.h"
+#include "funcoes_globais.h"
+#include "Classes/clcliente.h"
+#include "Classes/clveiculo.h"
 
 namespace Ui {
 class frm_ordemservico;
@@ -12,8 +26,19 @@ class frm_ordemservico : public QDialog
     Q_OBJECT
 
 public:
-    explicit frm_ordemservico(QWidget *parent = nullptr);
+    explicit frm_ordemservico(QWidget *parent = nullptr, QString c_codigo_cliente = ""
+                                                              , QString c_nome_cliente = ""
+                                                              , QString c_codigo_veiculo = ""
+                                                              , QString c_nome_veiculo = "");
     ~frm_ordemservico();
+
+    //instanciando conexao com o banco de dados
+    Conexao con;
+
+    QString g_codigo_cliente;
+    QString g_nome_cliente;
+    QString g_codigo_veiculo;
+    QString g_nome_veiculo;
 
 private:
     Ui::frm_ordemservico *ui;
